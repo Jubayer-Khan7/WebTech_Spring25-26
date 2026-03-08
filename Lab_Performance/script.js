18b1ae79eed5ac3140fa497de19679186e43f0f1
Jubayer-Khan7 <jubayermahi710@gmail.com>
2026-03-08 12:00:30 +0600

    last

--- Lab_Performance/script.js
+++ Lab_Performance/script.js
@@ -1,22 +1,22 @@
-function validation{
-    let isFNameValid = collect_name();
-}
-function collect_name(){
-    let name = document.getElementById("Sname").value;
-    console.log(name);
+// function validation{
+//     let isFNameValid = collect_name();
+// }
+// function collect_name(){
+//     let name = document.getElementById("Sname").value;
+//     console.log(name);
 
-    if(name == ""){
-        document.getElementById("namee").innerHTML = "Please fill the gap";
-         return false;
-    }
-     else if(name.length<5){
-        document.getElementById("namee").innerHTML = "Please write at least 4 character";
-      return false;
-    }
+//     if(name == ""){
+//         document.getElementById("namee").innerHTML = "Please fill the gap";
+//          return false;
+//     }
+//      else if(name.length<5){
+//         document.getElementById("namee").innerHTML = "Please write at least 4 character";
+//       return false;
+//     }
 
-    else{
-      document.getElementById("namee").innerHTML = "";
+//     else{
+//       document.getElementById("namee").innerHTML = "";
       
-    }
-  return false;
-}
+//     }
+//   return false;
+// }
